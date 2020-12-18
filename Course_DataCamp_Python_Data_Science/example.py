1547ae194fc8d54a2d9e1738261819843e631e01
mrm <matt.rmoore@gmail.com>
2020-12-17 20:25:12 -0600

    Preparing some items for Course 3

--- Course_DataCamp_Python_Data_Science/example.py
+++ Course_DataCamp_Python_Data_Science/example.py
@@ -6,3 +6,13 @@ def square_number(someNumber):
     return someNumber ** 2
 
 print(square_number(2))
+
+class MyClass:
+    def __init__(self, nameString, intValue):
+        self.nameString = nameString
+        self.intValue = intValue
+
+mc01 = MyClass("Name String", 52)
+
+print(mc01.nameString)
+print(mc01.intValue)
