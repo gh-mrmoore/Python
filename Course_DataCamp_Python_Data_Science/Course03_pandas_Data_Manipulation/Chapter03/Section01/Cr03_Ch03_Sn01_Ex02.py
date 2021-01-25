bed656a9c7e3aa402b3cbe19a1505d4c4b4e6aa4
mrm <matt.rmoore@gmail.com>
2021-01-25 17:19:52 -0600

    Complete thru Chapter 3, Section 1

---
+++ Course_DataCamp_Python_Data_Science/Course03_pandas_Data_Manipulation/Chapter03/Section01/Cr03_Ch03_Sn01_Ex02.py
@@ -0,0 +1,10 @@
+# Make a list of cities to subset on
+cities = ["Moscow", "Saint Petersburg"]
+
+#print(temperatures)
+
+# Subset temperatures using square brackets
+print(temperatures[temperatures["city"].isin(cities)])
+
+# Subset temperatures_ind using .loc[]
+print(temperatures_ind.loc[cities])
